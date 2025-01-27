26c4580681f1724fd15313a46d45c463040fa0d3
entsarahmed <entesara500@gmail.com>
2025-01-27 13:58:07 +0200

    Polymorphism

--- OOP01/Program.cs
+++ OOP01/Program.cs
@@ -7,70 +7,60 @@ namespace OOP01
 
         static void Main(string[] args)
         {
-            Dog dog01 = new Dog();
-            dog01.weight=100;
-            dog01.name ="roy";
-            dog01.Eat();
 
-            Elephant e1 = new Elephant();
-            e1.weight=1000;
+            Animal dog = new Dog();   //Reference from Parent refer to object from child ==> run Principle Polymorphism 
+            dog.MakeSound();
+            Animal cat = new Cat();
+            cat.MakeSound();
 
-            e1.Eat();
+
         }
     }
 
     // Base Class, Parent Class, Super Class 
 
-    class Animal
+    /// Polymorphism  => Support Inheritance
+
+    // Poly = Multiple - Many   * Morphism => Form
+    // Multiple Forms  =>أشكال متعدده => أعامل objectمختلفة على أنها نفس  object
+
+    class Animal //Base Class
     {
-        public string name;
-        public int weight;
-
-        public virtual void Eat()
+        public virtual void MakeSound()
         {
-            Console.WriteLine("Is eating ");
+            Console.WriteLine("Animal Make Sound");
         }
+       
     }
-  
-    //Derived Classes 
-
     class Dog : Animal
     {
-
-        public override void Eat()
+      public override void MakeSound()
         {
-            Console.WriteLine("Dog is eating");
+            Console.WriteLine("Dog Make Sound");
         }
-        public void Bark()
-        {
-            Console.WriteLine("dog is barking");
-        }
-
     }
-    class Elephant : Animal
+    class Cat : Animal
     {
-        public override void Eat()
+        public override void MakeSound()
         {
-            Console.WriteLine("Elephant is eating");
+            Console.WriteLine("Cat Make Sound");
         }
-        public void WalkingSlowly()
-        {
-            Console.WriteLine("dog is barking");
-        }
-        
     }
-
-    //sealed  ==> أنا كده بفهم الديفلوبر اللى هيجى إن ممنوع يشتق أو يورث من ال class ده
-    sealed class Garden //can't derive  From sealed type of "garden"  ==> No consider base class
-    {//No Make Child class ==> protect special Architecture --> Increase Optimization or performance with class
-        public int Area;
-        public string Address;
-
-    }
-    class Garden02
+    class Calculator
     {
-       
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
 
+        public double Add(double a, double b)   //Change Signature with Method
+        {
+            return a + b;
+        }
+        public double Add(double a, double b, double c)   //Change Signature with Method
+        {
+            return a + b + c;
+        }
     }
 
 }
