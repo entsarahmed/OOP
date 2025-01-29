763c7298f70a946ae0026008a41712dadfff3bc1
entsarahmed <entesara500@gmail.com>
2025-01-29 15:33:28 +0200

    NameSpaces & Accessability

--- OOP01/Program.cs
+++ OOP01/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.IO;
+using EmployeeLibrary;
+
 
 namespace OOP01
 {
@@ -7,56 +9,43 @@ namespace OOP01
 
         static void Main(string[] args)
         {
-            #region Read File  - 2 Ways ? the first way is the best?
+           //Name Space : -  هيكل تنظيمي للكود بتاعى  -- is a container 
+           // forbidden the Name Conflicts
+           //using / can skip the fully qualified in the Name Space -- and write Direct Class Name 
+         ///*  EmployeeLibrary.*/Employee employee = new EmployeeLibrary.Employee();
+         //   Salary salary = new Salary();
 
-            /*   StreamReader Sr = new StreamReader(@"F:\route.net\course\C# Practice\Test.txt");
-
-            //string line;
-
-            //while( (line = Sr.ReadLine()) != null)
-            //{
-            //    Console.WriteLine(line);
-            //}*/
-
-            /*  string[] result  = File.ReadLines(@"F:\route.net\course\C# Practice\Test.txt").ToArray<string>();
-               foreach (var line in result) 
-                   Console.WriteLine(line);*/
-
-            #endregion
-
-
-            #region Write New Log File in Document type of text?
-            string FilePath = @"F:\route.net\course\C# Practice\newFile.txt";
-
-            // 1.  select the store File lwrite in file line by line 
-            /*StreamWriter writer = new StreamWriter(FilePath);
-            for (int i = 0; i<5; i++)
-                writer.WriteLine("File input Video");
-            writer.Close();*/
-
-            //2. big Text write  all it once only
-            string contents = "\nNew Text 4";
-            //File.WriteAllText(FilePath, contents);
-
-           // File.AppendAllText(FilePath, contents);
-            
-            // Check on Exit file?
-            if (File.Exists(FilePath))
-                Console.WriteLine("File exist Already");
-
-
-
-
-            #endregion
-
-
+         //   EmployeeHR.Employee employeeHR = new EmployeeHR.Employee();
+         //   EmployeeLibrary.Employee employee1 = new EmployeeLibrary.Employee();
+       
+            Employee employee = new Employee();
         }
 
 
     }
-    // Files?
-   //أزاى أقرأ فايل ؟
-   // أول حاجة لازم أعرف مكانه فين
-   //Stream ==> Collection of Binary Data ==> Flow From Data ==> Flow From Bytes
-
+   
 }
+
+// Currently -> have Two Class Employee in different Name Space
+// How to Select Employee in different Name Space
+
+//namespace EmployeeLibrary
+//{
+//class Employee
+//    {
+
+//    }
+//    class Salary
+//    {
+
+//    }
+//}
+
+//namespace EmployeeHR
+//{
+//    class Employee
+//    {
+
+//    }
+
+//}
